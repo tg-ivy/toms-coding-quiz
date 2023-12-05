1bf373a9b9813d8931541fd5634347c93c1609ad
Thomas Gledhill <thomaspg@live.co.uk>
2023-12-05 15:23:41 +0000

    add questions

---
+++ assets/js/logic.js
@@ -0,0 +1,56 @@
+const questions = [
+    {
+        question: "What is a div?",
+        answers: [
+            {text: "tag", correct: true},
+            {text: "function", correct: false},
+            {text: "language", correct: false},
+            {text: "variable", correct: false}
+        ]
+    },
+    {
+        question: "How would you access the first item in an array?",
+        answers: [
+            {text: "array{0}", correct: false},
+            {text: "array.first", correct: false},
+            {text: "array[1]", correct: false},
+            {text: "array[0]", correct: true}
+        ]
+    },
+    {
+        question: "What selector accesses a class in CSS?",
+        answers: [
+            {text: ".", correct: true},
+            {text: "#", correct: false},
+            {text: "p", correct: false},
+            {text: ">", correct: false}
+        ]
+    },
+    {
+        question: "Which of the following statements is correct?",
+        answers: [
+            {text: "Const, let, and var can all be re-assigned", correct: false},
+            {text: "Let variables and const variables have different scopes", correct: false},
+            {text: "Var only declares variables, not objects", correct: false},
+            {text: "Const cannot be re-assigned", correct: true}
+        ]
+    },
+    {
+        question: "What does HTML stand for?",
+        answers: [
+            {text: "Hypertext Markdown Language", correct: false},
+            {text: "Hypertext Markup Language", correct: true},
+            {text: "Hardware To Motherboard Link", correct: false},
+            {text: "Highest Test Mark of Linux", correct: false}
+        ]
+    },
+
+];
+
+
+// ----------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------
+
+
+
+
